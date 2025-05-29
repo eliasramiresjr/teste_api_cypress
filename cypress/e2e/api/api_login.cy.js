5a4f0bc0d76f78ef6473227b8daae0cd8bf068fb
eliasramiresjr <ramires_junior11@hotmail.com>
2025-05-29 19:18:47 -0400

    ♻️ refactor:Nome atualizado

---
+++ cypress/e2e/api/api_login.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress"/>
+
+describe('API test', () => {
+
+  beforeEach('Go to home page', () => {
+    cy.visit('/')
+  })
+
+  it('Should login in page', () => {
+    cy.api_login('fulano@qa.com', 'teste').then((response) => {
+      expect(response.status).to.equal(200)
+      expect(response.body.message).to.equal('Login realizado com sucesso')
+    })
+  })
+
+  it.only('Should validate incorrect password', () => {
+    cy.api_login('fulano@qa.com', 'SenhaIncorreta').then((response) => {
+      expect(response.status).to.equal(401)
+      expect(response.body.message).to.equal('Email e/ou senha inválidos')
+    })
+  })
+
+})
