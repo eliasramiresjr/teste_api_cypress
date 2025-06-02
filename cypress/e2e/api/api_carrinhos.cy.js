b59b83e773eb0654261f59129d9bda3d9c5245f8
eliasramiresjr <ramires_junior11@hotmail.com>
2025-06-02 18:20:26 -0400

    :test_tube: test: Testes da rota carrinhos

---
+++ cypress/e2e/api/api_carrinhos.cy.js
@@ -0,0 +1,103 @@
+import { faker } from "@faker-js/faker";
+
+const randomProduct = faker.word.words()
+const randomAlphanumeric = faker.string.alphanumeric(16)
+
+let token
+
+describe('API test cart', () => {
+
+    it('Should login in page', () => {
+        cy.api_login('fulano@qa.com', 'teste').then((response) => {
+            token = response.body.authorization
+        })
+    })
+
+    it('Should products in cart', () => {
+        cy.request({
+            method: 'GET',
+            url: 'carrinhos'
+        }).then((response) => {
+            cy.log(JSON.stringify(response.body))
+        })
+    })
+
+    // it('Should register a new product in cart', () => {
+    //     cy.request({
+    //         method: 'POST',
+    //         url: 'carrinhos',
+    //         headers: {
+    //             authorization: token
+    //         },
+    //         body:
+    //         {
+    //             "produtos": [
+    //                 {
+    //                     "idProduto": `${randomProduct}`,
+    //                     "quantidade": 1
+    //                 }
+    //             ]
+    //         }
+    //     })
+    // });
+
+    it('Try register two products in cart', () => {
+        cy.request({
+            method: 'POST',
+            url: 'carrinhos',
+            headers: {
+                authorization: token
+            },
+            body:
+            {
+                "produtos": [
+                    {
+                        "idProduto": `${randomProduct}`,
+                        "quantidade": 1
+                    }
+                ]
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.body.message).to.equal('Não é permitido ter mais de 1 carrinho')
+        })
+    });
+
+    it('Try register a new product in cart using invalid token', () => {
+        cy.request({
+            method: 'POST',
+            url: 'carrinhos',
+            body:
+            {
+                "produtos": [
+                    {
+                        "idProduto": `${randomProduct}`,
+                        "quantidade": 1
+                    }
+                ]
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.body.message).to.equal('Token de acesso ausente, inválido, expirado ou usuário do token não existe mais')
+        })
+    })
+
+    it('Should find a cart using ID', () => {
+        cy.request({
+            method: 'GET',
+            url: 'carrinhos/qbMqntef4iTOwWfg'
+        })
+    })
+
+    it('Try find a cart using invalid ID', () => {
+        cy.request({
+            method: 'GET',
+            url: `carrinhos/${randomAlphanumeric}`,
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.body.message).to.equal('Carrinho não encontrado')
+        })
+    })
+
+
+})
